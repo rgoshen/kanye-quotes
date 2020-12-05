8183388937b220b5919c7be1c5d78091b8ba3b48
Richard Goshen <rick.goshen@gmail.com>
2020-12-05 14:02:21 -0700

    corrected get_quote function

--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import requests
 
 def get_quote():
     """Uses the kanye.rest api to return a random kanye request."""
-    url = "https://api.kanye.rest"
     
-    response = requests.get(url)
+    response = requests.get("https://api.kanye.rest")
     response.raise_for_status()
     
     data = response.json()
     quote = data["quote"]
-    return quote
+    canvas.itemconfig(quote_text, quote)
 
 
 
@@ -21,7 +20,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text=f"{get_quote()}", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
